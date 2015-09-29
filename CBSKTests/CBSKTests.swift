3b9dcead2edb1f9f742b186738b051022d2f0293
Jamal Hashim <jamalhashim@usmba-2014l1429.westminster.net>
2015-09-29 09:45:14 -0400

    Initial Commit

---
+++ CBSKTests/CBSKTests.swift
@@ -0,0 +1,36 @@
+//
+//  CBSKTests.swift
+//  CBSKTests
+//
+//  Created by Jamal Hashim on 9/29/15.
+//  Copyright (c) 2015 Jamal Hashim. All rights reserved.
+//
+
+import UIKit
+import XCTest
+
+class CBSKTests: XCTestCase {
+    
+    override func setUp() {
+        super.setUp()
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+    
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+        super.tearDown()
+    }
+    
+    func testExample() {
+        // This is an example of a functional test case.
+        XCTAssert(true, "Pass")
+    }
+    
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measureBlock() {
+            // Put the code you want to measure the time of here.
+        }
+    }
+    
+}
